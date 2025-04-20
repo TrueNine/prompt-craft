1aef0311dbd45cb4a3c18ef356437f146d17472f
赵日天 <truenine304520@gmail.com>
2025-04-20 16:03:23 +0800

    feat: 在other模块中引入Markdown内容，新增获取内容的功能并输出至控制台，增强模块的功能性

--- src/bin/other.ts
+++ src/bin/other.ts
@@ -1,3 +1,8 @@
+import a from '@/templates/cursor/shared/all.md'
+import { getContent } from './utils'
+
 export function other(): void {
   console.log('other module')
+  const content = getContent(a)
+  console.log(content)
 }
